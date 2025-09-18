c8291821adc4e48816403c0dce10c60935491333
RIIRLB <ricardoirvinglb@gmail.com>
2025-09-18 15:47:30 -0600

    2circulos.py

---
+++ 2circulos.py
@@ -0,0 +1,13 @@
+# Segundo ejemplo
+# RILB
+
+from manim import *
+
+class circulos(Scene):
+    def construct(self):
+        circ_1 = Circle(radius=1)
+        circ_2 = Circle(radius=1.5, color=GREEN_B)
+        circ_3 = Circle(radius=1, color= BLUE_E, fill_opacity=0.5)
+        
+        grupo_circ = Group(circ_1, circ_2,circ_3).arrange(buff=1)
+        self.add(grupo_circ)
