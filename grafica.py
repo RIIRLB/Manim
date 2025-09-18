9f54a0af36c1438b2cc184405d3b6d6d7dfd8978
RIIRLB <ricardoirvinglb@gmail.com>
2025-09-18 14:33:37 -0600

    Update grafica.py

--- grafica.py
+++ grafica.py
@@ -1,26 +1,87 @@
 from manim import *
 
-class FollowingGraphCamera(MovingCameraScene):
+class SineCurveUnitCircle(Scene):
+    # contributed by heejin_park, https://infograph.tistory.com/230
     def construct(self):
-        self.camera.frame.save_state()
+        self.show_axis()
+        self.show_circle()
+        self.move_dot_and_draw_curve()
+        self.wait()
 
-        # create the axes and the curve
-        ax = Axes(x_range=[-1, 10], y_range=[-1, 10])
-        graph = ax.plot(lambda x: np.sin(x), color=BLUE, x_range=[0, 3 * PI])
+    def show_axis(self):
+        x_start = np.array([-6,0,0])
+        x_end = np.array([6,0,0])
 
-        # create dots based on the graph
-        moving_dot = Dot(ax.i2gp(graph.t_min, graph), color=ORANGE)
-        dot_1 = Dot(ax.i2gp(graph.t_min, graph))
-        dot_2 = Dot(ax.i2gp(graph.t_max, graph))
+        y_start = np.array([-4,-2,0])
+        y_end = np.array([-4,2,0])
 
-        self.add(ax, graph, dot_1, dot_2, moving_dot)
-        self.play(self.camera.frame.animate.scale(0.5).move_to(moving_dot))
+        x_axis = Line(x_start, x_end)
+        y_axis = Line(y_start, y_end)
 
-        def update_curve(mob):
-            mob.move_to(moving_dot.get_center())
+        self.add(x_axis, y_axis)
+        self.add_x_labels()
 
-        self.camera.frame.add_updater(update_curve)
-        self.play(MoveAlongPath(moving_dot, graph, rate_func=linear))
-        self.camera.frame.remove_updater(update_curve)
+        self.origin_point = np.array([-4,0,0])
+        self.curve_start = np.array([-3,0,0])
 
-        self.play(Restore(self.camera.frame))
+    def add_x_labels(self):
+        x_labels = [
+            MathTex(r"\pi"), MathTex(r"2 \pi"),
+            MathTex(r"3 \pi"), MathTex(r"4 \pi"),
+        ]
+
+        for i in range(len(x_labels)):
+            x_labels[i].next_to(np.array([-1 + 2*i, 0, 0]), DOWN)
+            self.add(x_labels[i])
+
+    def show_circle(self):
+        circle = Circle(radius=1)
+        circle.move_to(self.origin_point)
+        self.add(circle)
+        self.circle = circle
+
+    def move_dot_and_draw_curve(self):
+        orbit = self.circle
+        origin_point = self.origin_point
+
+        dot = Dot(radius=0.08, color=YELLOW)
+        dot.move_to(orbit.point_from_proportion(0))
+        self.t_offset = 0
+        rate = 0.25
+
+        def go_around_circle(mob, dt):
+            self.t_offset += (dt * rate)
+            # print(self.t_offset)
+            mob.move_to(orbit.point_from_proportion(self.t_offset % 1))
+
+        def get_line_to_circle():
+            return Line(origin_point, dot.get_center(), color=BLUE)
+
+        def get_line_to_curve():
+            x = self.curve_start[0] + self.t_offset * 4
+            y = dot.get_center()[1]
+            return Line(dot.get_center(), np.array([x,y,0]), color=YELLOW_A, stroke_width=2 )
+
+
+        self.curve = VGroup()
+        self.curve.add(Line(self.curve_start,self.curve_start))
+        def get_curve():
+            last_line = self.curve[-1]
+            x = self.curve_start[0] + self.t_offset * 4
+            y = dot.get_center()[1]
+            new_line = Line(last_line.get_end(),np.array([x,y,0]), color=YELLOW_D)
+            self.curve.add(new_line)
+
+            return self.curve
+
+        dot.add_updater(go_around_circle)
+
+        origin_to_circle_line = always_redraw(get_line_to_circle)
+        dot_to_curve_line = always_redraw(get_line_to_curve)
+        sine_curve_line = always_redraw(get_curve)
+
+        self.add(dot)
+        self.add(orbit, origin_to_circle_line, dot_to_curve_line, sine_curve_line)
+        self.wait(8.5)
+
+        dot.remove_updater(go_around_circle)
